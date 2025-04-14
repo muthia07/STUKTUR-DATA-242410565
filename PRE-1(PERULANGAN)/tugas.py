2f7a56f9f52b393880da8e60690e2c11e472e20c
muthia07 <ainiyyatia@gmail.com>
2025-04-13 20:59:03 -0700

    Delete PRE-1(PERULANGAN) directory

--- PRE-1(PERULANGAN)/tugas.py
+++
@@ -1,4 +0,0 @@
-# Membuat pola bintang seperti di papan tulis menggunakan range
-for i in range(1, 4):  # Loop akan berjalan 3 kali (untuk baris 1, 2, dan 3)
-    jumlah_bintang = 2 * i - 1
-    print("*" * jumlah_bintang)
